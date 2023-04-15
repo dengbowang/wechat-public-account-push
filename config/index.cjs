5f421609ab10163595a7545348c7bad3f99676a6
立方体Cube <76862195+dengbowang@users.noreply.github.com>
2023-04-15 22:01:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '00',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oLiUI6P3F8WcDwLqMvEHwrFtZMnU',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '5cu4xtpqodWp50UOqApiAQfWO1wJtXMp6nXM3mOWv7w',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -55,7 +55,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '03',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oLiUI6E_oBn7CZqBcotOugsG91B8',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '5cu4xtpqodWp50UOqApiAQfWO1wJtXMp6nXM3mOWv7w',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
